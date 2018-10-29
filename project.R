710a8cd7fed8e1dbe0a93e353d976155a0da96e6
Cesomark <ceso.mark@gmail.com>
2018-10-29 16:07:02 +0100

    firstPush

--- project.R
+++ project.R
@@ -1,3 +1,19 @@
+getYearInTitle <- function(title, folderName) {
+  titleIndex <- regexpr('[0-9][0-9][0-9][0-9]', folderName)[1]
+  if(titleIndex != -1) {
+    substr(folderName, titleIndex, titleIndex + 3)
+    
+  } else {
+    yearIndex <- regexpr('\\.[0-9][0-9][0-9][0-9]\\.', title)[1]
+    
+    if(yearIndex != -1) {
+      substr(title, yearIndex+1, yearIndex + 4)
+    } else {
+      NULL 
+    }
+  }
+}
+
 folderNames = list.files("./tracks", full.names = TRUE)
 songList = list()
 
@@ -10,5 +26,13 @@ for(i in 1:length(folderNames)){
 #songList[[30]]
 #--access 5th song in list 30:
 #songList[[30]][[1]][[5]]
+
+#x <- songList[[29]][[1]][[33]]
+x <- songList[[30]][[1]][[6]]
+x
+
+lol <- getYearInTitle(x, songList[[30]]$folderName)
+lol
+
 
 
