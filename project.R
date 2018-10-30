ed90162c0c895d437a7f364617f0fdd760a3a9a1
Cesomark <ceso.mark@gmail.com>
2018-10-30 14:41:53 +0100

    Sorting copies of songs into new folder

--- project.R
+++ project.R
@@ -1,7 +1,55 @@
-getYearInTitle <- function(title, folderName) {
-  titleIndex <- regexpr('[0-9][0-9][0-9][0-9]', folderName)[1]
+#-------- Helper Functions ----------
+initializeSongList <- function(folderNames) {
+  tempList <- list()
+  
+  for(i in 1:length(folderNames)){
+    tempList[[i]] <- list(list.files(folderNames[i]))
+    tempList[[i]]$folderName <- folderNames[[i]]
+  }
+  
+  tempList
+}  
+
+sortSongs <- function() {
+  #creating the destination folder once at the start
+  dir.create(file.path(getwd(), "sorted"), showWarnings = FALSE)
+  
+  for(i in 1:length(songList)) {
+    for(j in 1:length(songList[[i]][[1]])) {
+      
+      copySongToSortedFolder(songList[[i]][[1]][[j]], songList[[i]]$folderName)
+    
+    }
+  }
+}
+
+copySongToSortedFolder <- function(title, folder) {
+  createFolderForSortedSong(title, folder)
+
+  file.copy(from=file.path(getwd(), getOldSongPath(title, folder)), 
+            to=file.path(getwd(), getNewSongPath(title, folder)), 
+            overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+}
+
+createFolderForSortedSong <- function(title, folder) {
+  year <- getYearOfTrack(title, folder)
+  location <- paste("sorted", year, sep="/")
+  dir.create(file.path(getwd(), location), showWarnings = FALSE)
+}
+
+getOldSongPath <- function(title, folder) {
+  paste(folder, title, sep="/")
+}
+
+getNewSongPath <- function(title, folder) {
+  year <- getYearOfTrack(title, folder)
+  paste("sorted", year, title, sep="/")
+}
+
+getYearOfTrack <- function(title, folder) {
+  titleIndex <- regexpr('[0-9][0-9][0-9][0-9]', folder)[1]
   if(titleIndex != -1) {
-    substr(folderName, titleIndex, titleIndex + 3)
+    substr(folder, titleIndex, titleIndex + 3)
     
   } else {
     yearIndex <- regexpr('\\.[0-9][0-9][0-9][0-9]\\.', title)[1]
@@ -9,30 +57,28 @@ getYearInTitle <- function(title, folderName) {
     if(yearIndex != -1) {
       substr(title, yearIndex+1, yearIndex + 4)
     } else {
-      NULL 
+      3000 
     }
   }
 }
+#-------------------------------------------------------
 
-folderNames = list.files("./tracks", full.names = TRUE)
-songList = list()
 
-for(i in 1:length(folderNames)){
-  songList[[i]] <- list(list.files(folderNames[i]))
-  songList[[i]]$folderName <- folderNames[[i]]
-  
-}
+folderNames <- list.files("./tracks", full.names = TRUE)
+songList <- initializeSongList(folderNames)
+
+sortSongs()
+
+
+
+
+#---------- Stuff -------------------
+#--Get folder name for 30th list:
 #songList[[30]]$folderName
+#--Get folder content for 30th list:
 #songList[[30]]
-#--access 5th song in list 30:
+#--Get 5th song in list 30:
 #songList[[30]][[1]][[5]]
-
-#x <- songList[[29]][[1]][[33]]
-x <- songList[[30]][[1]][[6]]
-x
-
-lol <- getYearInTitle(x, songList[[30]]$folderName)
-lol
 
 
 
